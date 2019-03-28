cadf91b7280d115b984e187cf405f64e266d5081
Arisht Jain <41357236+jainarisht@users.noreply.github.com>
2019-03-28 16:03:05 +0530

    Update Counter.sol

--- Counter.sol
+++ Counter.sol
@@ -1,44 +1,18 @@
-pragma solidity ^0.4.0;
-
-contract GetSetContract{
-
-    mapping (string => string) state;
-    event StateSaveEvent(string key, string value);
-
-    function get(string memory key) public view returns(string) {
-        string memory value = state[key];
-        
-        require(!equal(value, ''));
-        
-        return value;
-    }
-
-    function set(string memory key, string memory value) public {
-        state[key] = value;
-
-        emit StateSaveEvent(key, value);
-    }
+pragma solidity ^0.4.23;
+contract Counter {
     
-    function compare(string _a, string _b) internal returns (int) {
-        bytes memory a = bytes(_a);
-        bytes memory b = bytes(_b);
-        uint minLength = a.length;
-        if (b.length < minLength) minLength = b.length;
-        
-        for (uint i = 0; i < minLength; i ++)
-            if (a[i] < b[i])
-                return -1;
-            else if (a[i] > b[i])
-                return 1;
-        if (a.length < b.length)
-            return -1;
-        else if (a.length > b.length)
-            return 1;
-        else
-            return 0;
+    event CounterIncrementedEvent(int count);
+    event CounterDecrementedEvent(int count);
+    int private count = 0;
+    function incrementCounter() public {
+        count += 1;
+        emit CounterIncrementedEvent(count);
     }
-    
-    function equal(string _a, string _b) internal returns (bool) {
-        return compare(_a, _b) == 0;
+    function decrementCounter() public {
+        count -= 1;
+        emit CounterDecrementedEvent(count);
+    }
+    function getCount() public constant returns (int) {
+        return count;
     }
 }
